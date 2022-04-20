35de9fa30f9c50c1166f522b0545e74fd184ef68
CXX20 <59842069+CXX20@users.noreply.github.com>
2022-04-20 10:42:46 +0300

    rethink Vec layout

--- fixx/vec.hpp
+++ fixx/vec.hpp
@@ -5,34 +5,27 @@
 #include <memory>
 
 namespace fixx {
-	template<typename T, typename N = std::size_t> class Vec {
+	template<typename T> class Vec {
 		static_assert(std::is_trivially_destructible_v<T>);
 
-		T* arr{};
-		std::size_t len{};
-		[[no_unique_address]] N cap{};
+		T *from{}, *to{};
 
 	public:
-		template<typename M = N, typename = std::enable_if_t<!Metaval<M>>>
-		constexpr Vec()
-		{}
-		template<typename B, typename = decltype(arr = std::declval<B>().data())>
+		constexpr Vec() {}
+		template<typename B, typename = decltype(from = std::declval<B>().data())>
 		constexpr Vec(B&& buf)
-		: arr{std::forward<B>(buf).data()}, cap{buf.size()} {}
+		: from{std::forward<B>(buf).data()}, to{from} {}
 
-		constexpr auto begin() const { return arr; }
-		constexpr auto end() const { return arr + len; }
-		constexpr auto size() const { return len; }
+		constexpr auto begin() const { return from; }
+		constexpr auto end() const { return to; }
+		constexpr auto size() const { return std::size_t(to - from); }
 		template<typename I> constexpr auto& operator[](I i) const
-		{ return constexpr_assert(i < len), constexpr_assert(i < cap), arr[i]; }
-		template<typename... As> constexpr auto& emplace_back(As&&... args) & {
-			constexpr_assert(len < cap);
-			std::construct_at(arr + len, std::forward<As>(args)...);
-			return arr[len++];
-		}
+		{ return constexpr_assert(i < size()), from[i]; }
+		template<typename... As> constexpr auto& emplace_back(As&&... args) &
+		{ return std::construct_at(to, std::forward<As>(args)...), *to++; }
 	};
 	template<typename B> Vec(B&& buf)
-	-> Vec<std::remove_pointer_t<decltype(buf.data())>, decltype(buf.size())>;
+	-> Vec<std::remove_pointer_t<decltype(buf.data())>>;
 }
 
 #endif
