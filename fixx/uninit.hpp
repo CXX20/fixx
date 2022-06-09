37a517eadf5367f149db0bf6d427addd61d33d4d
CXX20 <59842069+CXX20@users.noreply.github.com>
2022-06-09 22:31:42 +0300

    add Uninit::reset, specialize for T[n]

--- fixx/uninit.hpp
+++ fixx/uninit.hpp
@@ -1,6 +1,7 @@
 #ifndef FIXX_HEADER_UNINIT
 #define FIXX_HEADER_UNINIT
 
+#include "core.hpp"
 #include <memory>
 
 namespace fixx {
@@ -20,6 +21,8 @@ namespace fixx {
 		template<typename... As> constexpr auto& emplace(As&&... args) &
 		{ return *std::construct_at(ptr(), std::forward<As>(args)...); }
 
+		constexpr auto& reset() & { return std::destroy_at(ptr()), *this; }
+
 		constexpr auto& operator*() const& { return *ptr(); }
 		constexpr auto& operator*() & { return *ptr(); }
 		auto operator*() && = delete;
@@ -27,6 +30,30 @@ namespace fixx {
 		constexpr auto operator->() const& { return ptr(); }
 		constexpr auto operator->() & { return ptr(); }
 		auto operator->() && = delete;
+	};
+	template<typename T, std::size_t n> class Uninit<T[n]> {
+		union { T rt[n], *ct{}; };
+
+		static constexpr auto is_ct() { return std::is_constant_evaluated(); }
+	public:
+		static constexpr auto size() { return value<n>; }
+
+		constexpr Uninit() { if (is_ct()) ct = std::allocator<T>{}.allocate(n); }
+		Uninit(Uninit&&) = delete;
+		constexpr ~Uninit() { if (is_ct()) std::allocator<T>{}.deallocate(ct, n); }
+
+		template<typename I, typename... As>
+		constexpr auto& emplace(I idx, As&&... args) & {
+			return constexpr_assert(idx < size()),
+				*std::construct_at(data() + idx, std::forward<As>(args)...);
+		}
+
+		template<typename I> constexpr auto& erase(I i) &
+		{ return constexpr_assert(i < size()), std::destroy_at(data() + i), *this; }
+
+		constexpr auto data() const& { return is_ct() ? ct : rt; }
+		constexpr auto data() & { return is_ct() ? ct : rt; }
+		auto data() && = delete;
 	};
 }
 
--- test/uninit.cpp
+++ test/uninit.cpp
@@ -2,7 +2,10 @@
 #include "../fixx/buf.hpp"
 
 namespace {
-	using fixx::Uninit;
+	using fixx::Uninit, fixx::operator""_c;
+
+	static_assert(!std::is_move_constructible_v<Uninit<int>>);
+	static_assert(!std::is_move_constructible_v<Uninit<int[42]>>);
 
 	static_assert([u = Uninit<int>{}]() mutable { return u.emplace(7) == 7; }());
 	static_assert([u = Uninit<int>{}] { return std::to_address(u); }());
@@ -10,6 +13,19 @@ namespace {
 	static_assert((Uninit<fixx::Buf<int>>{}, true));
 	static_assert([u = Uninit<int>{}]() mutable { return u.emplace(42); }());
 	static_assert([u = Uninit<fixx::Buf<int>>{}]() mutable {
-		return u.emplace(42), std::destroy_at(std::to_address(u)), true;
+		return u.emplace(42), u.reset(), true;
 	}());
+
+	static_assert(Uninit<int[3]>::size().value == 3);
+	static_assert([u = Uninit<int[3]>{}] { return u.data(); }());
+	static_assert([u = Uninit<int[3]>{}]() mutable { return u.data()[2] = 7; }());
+	static_assert([] {
+		Uninit<fixx::Buf<int>[3]> u;
+		auto ret = u.emplace(1_c, 42).data();
+		return u.erase(1_c), ret;
+	}());
+
+	struct Arg { constexpr Arg(int) {} };
+
+	static_assert([u = Uninit<Arg>{}]() mutable { return u.emplace(7), true; }());
 }
